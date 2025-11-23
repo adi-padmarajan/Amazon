3e8fb6a8f29b58d265992fabfc84ac4dea28e3d5
Aditya Padmarajan <aditya.padmarajan@gmail.com>
2025-11-23 05:37:04 -0800

    Made the cart quantity in the cart icon interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -73,11 +73,21 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       matchingItem.quantity += 1;
     }
     else{
-      cart.push({
-      productId: productId,
-      quantity: 1
-    });
+        cart.push({
+        productId: productId,
+        quantity: 1
+      });
     }
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    console.log(cartQuantity);
     console.log(cart);
   });
 });
