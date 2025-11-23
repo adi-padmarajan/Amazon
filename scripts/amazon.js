c86844626dc6dd7bdca341806579be56bfe6a7b4
Aditya Padmarajan <aditya.padmarajan@gmail.com>
2025-11-23 05:08:59 -0800

    Added data attribute to the products to uniquely identify each product when adding to cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,8 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-name = "${product.name}">
             Add to Cart
           </button>
         </div>
@@ -57,7 +58,7 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
-    console.log('Added Product');
+    console.log(button.dataset.productName);
   })
 });
 
