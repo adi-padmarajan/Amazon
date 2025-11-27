bfc0a3d3f9c9a18c352edba9358dfbe7479389b1
Aditya Padmarajan <aditya.padmarajan@gmail.com>
2025-11-27 08:35:16 -0800

    Moved the updateCartQuantity to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,25 @@
 export const cart = [];
+
+export function addToCart(productId){
+   //Check if the product is already in the cart
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+      if(productId === cartItem.productId){
+        matchingItem = cartItem;
+      }
+    });
+
+    //If the product is already in the cart, increase its quantity by 1, else push to cart.
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+    const quantity = Number(quantitySelector.value);
+    if(matchingItem){
+      matchingItem.quantity += quantity;
+    }
+    else{
+        cart.push({
+        productId,
+        quantity
+      });
+    }
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,31 +1,7 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let productsHTML = '';
-
-function addToCart(productId){
-   //Check if the product is already in the cart
-    let matchingItem;
-
-    cart.forEach((cartItem) => {
-      if(productId === cartItem.productId){
-        matchingItem = cartItem;
-      }
-    });
-
-    //If the product is already in the cart, increase its quantity by 1, else push to cart.
-    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-    const quantity = Number(quantitySelector.value);
-    if(matchingItem){
-      matchingItem.quantity += quantity;
-    }
-    else{
-        cart.push({
-        productId,
-        quantity
-      });
-    }
-}
 
 products.forEach((product) => {
     productsHTML += `
