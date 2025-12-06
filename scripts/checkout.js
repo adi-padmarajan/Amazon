52eac516da36c968808221f10afa187b5ce36749
Aditya Padmarajan <aditya.padmarajan@gmail.com>
2025-12-05 18:06:53 -0800

    Added dayjs external library with some code cleanup

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import {cart, removeFromCart, updateQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
-hello();
+console.log(dayjs());
 
 let cartSummaryHTML = '';
 
